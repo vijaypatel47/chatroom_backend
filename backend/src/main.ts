ab9a75528cbf45620f6138faf06fb1b0b5b79508
vijay <vp3828168@gmail.com>
2024-09-25 12:53:21 +0530

    prisma file update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://chat-room-demo-three.vercel.app', // Frontend URL
+    origin: 'http://localhost:3000', // Frontend URL
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization'
   });
