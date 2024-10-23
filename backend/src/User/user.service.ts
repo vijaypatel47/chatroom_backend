9295e511822d8e2803cb876fcdd76338f939f22a
vijay <vijay.patel@mobilefirstapplications.com>
2024-10-23 15:48:26 +0530

    try catch used in login and reg

--- backend/src/User/user.service.ts
+++ backend/src/User/user.service.ts
@@ -8,19 +8,29 @@ export class UserService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   async register(username: string, password: string) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    return await this.prisma.user.create({
+    try{
+      const hashedPassword = await bcrypt.hash(password, 10);
+      return await this.prisma.user.create({
       data: { username, password: hashedPassword },
     });
+    }
+    catch{
+      return 'Internal Server Error'
+    }
   }
 
   async login(username: string, password: string) {
-    const user = await this.prisma.user.findUnique({ where: { username } });
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    try{
+      const user = await this.prisma.user.findUnique({ where: { username } });
+      if (!user || !(await bcrypt.compare(password, user.password))) {
       return  new Error('Invalid credentials');
     }
-    const payload = { username: user.username };
+      const payload = { username: user.username };
     // console.log({ access_token: this.jwtService.sign(payload) })
-    return { access_token: this.jwtService.sign(payload) };
+      return { access_token: this.jwtService.sign(payload) };
+    }
+    catch{
+      return 'Internal Server Error'
+    }
   }
 }
